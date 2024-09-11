28b06ecb33b9162635b2df7ad7da5a6139cb1b72
Viraat Das <viraat.laldas@gmail.com>
2024-09-10 22:49:37 -0700

    writing scraper

---
+++ lexnet/scraper/scrape.py
@@ -0,0 +1,15 @@
+import os
+
+
+root_dir = "../data"
+
+processed = []
+
+with open("scraped.txt", "r") as file:
+    content = file.read()
+    processed = [line.strip() for line in content.splitlines()]
+  
+
+
+for dirpath, dirnames, filenames in os.walk(root_dir):
+    
